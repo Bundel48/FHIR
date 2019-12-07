99b6b749de1a524fac678a91ef3558d95811d264
Finja Ottink <finja.ottink@student.uni-luebeck.de>
2019-12-08 00:54:53 +0100

    added and changed things in encounter, organization, immunization and added comments

--- IntellijProject/MIOProjektImpfausweis/src/main/java/IntellijProject/MIOProjektImpfausweis/src/main/java/Impfausweis.java
+++ IntellijProject/MIOProjektImpfausweis/src/main/java/IntellijProject/MIOProjektImpfausweis/src/main/java/Impfausweis.java
@@ -4,8 +4,7 @@ import ca.uhn.fhir.context.FhirContext;
 import ca.uhn.fhir.rest.client.api.IGenericClient;
 import org.hl7.fhir.r4.model.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Date;
 import java.util.UUID;
 
 public class Impfausweis {
@@ -21,20 +20,25 @@ public class Impfausweis {
 
 
         Patient patient = createPatient();
-        Encounter encounter1 = createEncounter(patient);
-        Practitioner practitioner1 = createPractitioner();
+        // TODO mehr practitioner: z.B. Kinderarzt und Arzt
+        Practitioner practitioner = createPractitioner();
+        // TODO mehrere Organisationen: z.B. Krankenhaus oder Impfstelle, Kinderarztpraxis, vaccinationOrganization
         Organization organization = createOrganization();
-        PractitionerRole practitionerRole = createPractitionerRole(organization, practitioner1);
-        Immunization vac1 = createVaccination(patient, encounter1, practitioner1, "IFPA", "urn:oid:1.2.36.1.2001.1005.17", "Infanrix Penta","1997-08-12");
+        // TODO Encounter für jede einzelne Impfung anlegen
+        Encounter encounter = createEncounter(patient, practitioner, organization);
+        // TODO Rollen für jeden Practitioner
+        PractitionerRole practitionerRole = createPractitionerRole(organization, practitioner);
+        // TODO mehr Impfungen anlegen
+        Immunization immunization = createVaccination(patient, encounter, practitioner, "IFPA", "urn:oid:1.2.36.1.2001.1005.17", "Infanrix Penta","1997-08-12");
 
 
         Bundle bundle = new Bundle();
         bundle.addEntry().setResource(patient).getRequest().setUrl(patient.fhirType()).setMethod(Bundle.HTTPVerb.POST);
-        bundle.addEntry().setResource(encounter1).getRequest().setUrl(encounter1.fhirType()).setMethod(Bundle.HTTPVerb.POST);
-        bundle.addEntry().setResource(practitioner1).getRequest().setUrl(practitioner1.fhirType()).setMethod(Bundle.HTTPVerb.POST);
+        bundle.addEntry().setResource(encounter).getRequest().setUrl(encounter.fhirType()).setMethod(Bundle.HTTPVerb.POST);
+        bundle.addEntry().setResource(practitioner).getRequest().setUrl(practitioner.fhirType()).setMethod(Bundle.HTTPVerb.POST);
         bundle.addEntry().setResource(organization).getRequest().setUrl(organization.fhirType()).setMethod(Bundle.HTTPVerb.POST);
         bundle.addEntry().setResource(practitionerRole).getRequest().setUrl(practitionerRole.fhirType()).setMethod(Bundle.HTTPVerb.POST);
-        bundle.addEntry().setResource(vac1).getRequest().setUrl(vac1.fhirType()).setMethod(Bundle.HTTPVerb.POST);
+        bundle.addEntry().setResource(immunization).getRequest().setUrl(immunization.fhirType()).setMethod(Bundle.HTTPVerb.POST);
 
 
         // Parser to encode the resource into a string in json format
@@ -67,9 +71,13 @@ public class Impfausweis {
                 .setFamily("Reid")
                 .addGiven("Erik");
 
-        // Set birth date of the patient object
-        patient.setBirthDateElement(new DateType("1995-05-17"));
+        // Set gender of the patient object
+        patient.setGender(Enumerations.AdministrativeGender.MALE);
 
+        // Set birth date of the patient object
+        patient.setBirthDateElement(new DateType("1997-05-17"));
+
+        // Set address date of the patient object
         patient.addAddress()
                 .setUse(Address.AddressUse.HOME)
                 .setType(Address.AddressType.BOTH)
@@ -78,10 +86,11 @@ public class Impfausweis {
                 .setPostalCode("23552")
                 .setCountry("DE");
 
+        // Set birthplace of the patient object
         Extension ext = new Extension();
         ext.setUrl("http://hl7.org/fhir/StructureDefinition/patient-birthPlace");
         Address birthplace = new Address();
-        birthplace.setCity("Kiel");
+        birthplace.setCity("Kiel").setCountry("DE");
         ext.setValue(birthplace);
         patient.addExtension(ext);
 
@@ -92,15 +101,19 @@ public class Impfausweis {
 
 
     public static Immunization createVaccination(Patient patient, Encounter encounter, Practitioner practitioner, String vacCode, String vacSystem, String vacDisplay, String date) {
-        Immunization vac = new Immunization();
+        // Create an immunization object
+        Immunization immunization = new Immunization();
 
-        vac.addIdentifier()
+        // Set Identifier of the immunization object
+        immunization.addIdentifier()
                 .setSystem("http://www.kh-uzl.de/fhir/vaccination")
                 .setValue(UUID.randomUUID().toString());
 
-        vac.setStatus(Immunization.ImmunizationStatus.COMPLETED);
+        // Set status of the immunization object
+        immunization.setStatus(Immunization.ImmunizationStatus.COMPLETED);
 
-        vac.setVaccineCode(new CodeableConcept()
+        // Set VaccineCode of the immunization object
+        immunization.setVaccineCode(new CodeableConcept()
             .addCoding(new Coding()
                     .setCode(vacCode)
                     .setSystem(vacSystem)
@@ -108,35 +121,36 @@ public class Impfausweis {
             )
         );
 
-        vac.setPatient(new Reference()
+        // Set reference to the patient belonging to the immunization
+        immunization.setPatient(new Reference()
                 .setIdentifier(patient.getIdentifierFirstRep())
                 .setReference(patient.fhirType() + "/" +patient.getId()));
 
-        vac.setEncounter(new Reference()
+        // Set reference to the encounter belonging to the immunization
+        immunization.setEncounter(new Reference()
                 .setIdentifier(encounter.getIdentifierFirstRep())
                 .setReference(encounter.fhirType() + "/" + encounter.getId()));
 
-        vac.setOccurrence(new DateTimeType(date));
+        // Set vaccine administration date
+        immunization.setOccurrence(new DateTimeType(date));
 
         //Location
         //Manufacturer
 
-        //lotNumber
-        vac.setLotNumber("S2409F");
+        //Set lotNumber
+        immunization.setLotNumber("S2409F");
 
-        //*performer -> Practitioner
-        vac.addPerformer().setActor(new Reference()
+        // Set reference to the actor(practitioner) who performed the immunization
+        immunization.addPerformer().setActor(new Reference()
                 .setIdentifier(practitioner.getIdentifierFirstRep())
                 .setReference(practitioner.fhirType() + "/" + practitioner.getId()));
 
-
-
-        return vac;
+        return immunization;
     }
 
 
 
-    public static Encounter createEncounter(Patient patient){
+    public static Encounter createEncounter(Patient patient, Practitioner practitioner, Organization organization){
         // Create an encounter object
         Encounter encounter = new Encounter();
 
@@ -145,25 +159,41 @@ public class Impfausweis {
                 .setSystem("http://www.kh-uzl.de/fhir/encounter")
                 .setValue(UUID.randomUUID().toString());
 
+        // Set Status of the encounter object
         encounter.setStatus(Encounter.EncounterStatus.FINISHED);
 
-        //TODO Code eventuell auch AMB mit display value ambulatory
+        // Set classification of the encounter object
         encounter.setClass_(new Coding()
-                .setCode("NONAC")
+                .setCode("AMB")
                 .setSystem("http://terminology.hl7.org/CodeSystem/v3-ActCode")
-                .setDisplay("inpatient non-acute"));
+                .setDisplay("ambulatory"));
 
+        // Set specific type of service of the encounter object
         encounter.setServiceType(new CodeableConcept()
                 .addCoding(new Coding()
                         .setCode("57")
                         .setSystem("http://terminology.hl7.org/CodeSystem/service-type")
                         .setDisplay("Immunization")));
 
+        // Set reference to the patient that was present at the encounter
         encounter.setSubject(new Reference()
                 .setIdentifier(patient.getIdentifierFirstRep())
                 .setReference(patient.fhirType() + "/" + patient.getId()));
 
-        //TODO Arzt und Zeit irgendwie hinzufügen
+        // Set reference to the practitioner who was involved in the encounter
+        encounter.addParticipant().setIndividual(new Reference()
+                .setIdentifier(practitioner.getIdentifierFirstRep())
+                .setReference(practitioner.fhirType() + "/" + practitioner.getId()));
+
+        // Set period when the immunization took place
+        encounter.setPeriod(new Period()
+                .setStartElement(new DateTimeType("1997-08-12"))
+                .setEndElement(new DateTimeType("1997-08-12")));
+
+        // Set reference to the organization that is responsible for the encounter
+        encounter.setServiceProvider(new Reference()
+                .setIdentifier(organization.getIdentifierFirstRep())
+                .setReference(organization.fhirType() + "/" + organization.getId()));
 
         return encounter;
     }
@@ -171,18 +201,22 @@ public class Impfausweis {
 
 
     public static Practitioner createPractitioner(){
+        // Create a practitioner object
         Practitioner practitioner  =  new Practitioner();
 
+        // Set Identifier of the practitioner object
         practitioner.addIdentifier()
                 .setSystem("http://www.kh-uzl.de/fhir/practitioner")
                 .setValue(UUID.randomUUID().toString());
 
+        // Set name of the practitioner object
         practitioner.addName()
                 .setUse(HumanName.NameUse.OFFICIAL)
                 .setFamily("Wolf")
                 .addGiven("Lucas")
-                .addPrefix("Dr");
+                .addPrefix("Dr.");
 
+        // Set qualification of the practitioner object
         practitioner.addQualification().setCode(new CodeableConcept()
                 .addCoding(new Coding()
                         .setCode("MD")
@@ -195,27 +229,41 @@ public class Impfausweis {
 
 
     public static Organization createOrganization(){
-        // Krankenhaus
+        // Create an organization object
         Organization organization= new Organization();
 
+        // Set Identifier of the organization object
         organization.addIdentifier()
                 .setSystem("http://www.kh-uzl.de/fhir/organization")
                 .setValue(UUID.randomUUID().toString());
 
+        // Set if the organization's record is still in active use
+        organization.setActive(true);
+
+        // Set kind of organization
+        organization.addType(new CodeableConcept()
+                .addCoding(new Coding()
+                        .setCode("prov")
+                        .setSystem("http://terminology.hl7.org/CodeSystem/organization-type")
+                        .setDisplay("Healthcare Provider")));
+
+        // Set name of the organization object
         organization.setName("MIO Krankenhaus");
 
+        // Set address of the organization object
         organization.addAddress()
                 .setCity("Lübeck")
                 .setCountry("Deutschland")
                 .setPostalCode("23562");
+        //TODO evtl. addLine
 
         return organization;
     }
 
 
-    
+
     public static PractitionerRole createPractitionerRole(Organization organization, Practitioner practitioner){
-        // Rollenzuweisung
+        // Create practitionerRole
         PractitionerRole practitionerRole =
                 new PractitionerRole()
                         .setOrganization(new Reference()
@@ -227,9 +275,5 @@ public class Impfausweis {
 
         return practitionerRole;
     }
-
-
-
-
 
 }
